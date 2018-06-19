743e99c6d3a583283efe1f40a905c808cf0c36e9
Rudda <beltrao.rudah@gmail.com>
2018-06-19 10:22:55 -0400

    drawer left with bug

--- app/src/main/java/br/dev/app/jerab/multiplesdrawers/MainActivity.java
+++ app/src/main/java/br/dev/app/jerab/multiplesdrawers/MainActivity.java
@@ -1,14 +1,56 @@
 package br.dev.app.jerab.multiplesdrawers;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Toolbar tb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout =(DrawerLayout) findViewById(R.id.drawer);
+        drawerLayout.closeDrawers();
+        tb = (Toolbar) findViewById(R.id.toolbar);
+
+
+
+        setSupportActionBar(tb);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                tb, R.string.app_name, R.string.app_name) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle("closed");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle("opened");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        drawerLayout.setDrawerListener(mDrawerToggle);
+
+
 
         //comments
     }
